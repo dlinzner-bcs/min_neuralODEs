4b09ae2f6d30897ad3b6b12c50b56cc433f4947c
Dominik Linzner <dominik.linzner@bcs.tu-darmstadt.de>
2020-08-28 12:28:21 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import autograd.numpy as np
+import matplotlib.pyplot as plt
+import copy
+from scipy.integrate import solve_ivp
+
+from neuralODEs.neuralODE import neuralODE
+
+def f_true(t,x): #ground truth ode
+    A = - 1*np.array([[0.1,1],[1,0.1]])
+    y = A@x
+    return y
+
+def lin_layer(x, w):  #linear ode
+    return w@x
+
+def tanh_layer(x, w): #single tanh layer ode
+    a = w[:,:-1].T @ x
+    y = np.tanh(a+ w[:,-1])
+    return y
+
+def one_hl(x, w):  #single hiddenlayer neural ode
+    w1   = w[0:2,:]
+    w2   = w[2:4, :]
+    b1   = w[4,:]
+    b2   = w[5,0:2]
+
+    h = np.tanh(w1.T@x+b1)
+    y = w2@h+b2
+    return y
+
+if __name__ == "__main__":
+
+    M = 11 #number of time stencils
+    t0, t1 = 0, 1 #start and endpoint
+    x0s = np.random.normal(0, 1, (1000,2))  # 1000 random points in 2D
+
+    #neuralODE
+    D_in,K = 2 ,10 #D_in is ODE dim, K is dimension of hidden layer
+    weights =  np.random.normal(0,0.1,size=(2*D_in+2,K))
+    f = lambda x,w: one_hl(x,w)
+
+    #linearODE #for linearODE just comment out
+   # weights = np.random.normal(0, 0.1, size=(2,2))
+   # f = lambda x, w: lin_layer(x, w)
+
+    x1s = copy.copy(x0s)
+    for k in range(0, len(x0s)):
+        sol = solve_ivp(f_true, [t0, t1], x0s[k, :].flatten())
+        x = sol.y[:, -1]
+        x1s[k, :] = x
+
+    node = neuralODE(f, weights, x0s, x1s, t0, t1, M, 0.01)
+    for i in range(0,1000):
+        loss = node.train_step() #stochastic gradient descent
+        # gt solution
+        sol = solve_ivp(f_true, [node.t0, node.t1], y0 = x0s[-1,:].flatten(),t_eval=node.t_z)
+
+        if i % 10 ==0:
+            print(i)
+            plt.clf()
+            plt.plot(node.t_z,node.z.T[:, 0],'r',label='neuralODE')
+            plt.plot(node.t_z,sol.y[0,:],'b',label='ground truth')
+            plt.plot(node.t_a, node.a.T[:, 1], 'g' ,label='adjoint')
+            plt.plot(node.t_z, node.z.T[:, 1],'r')
+            plt.plot(node.t_z, sol.y[1, :],'b')
+            plt.plot(node.t_a, node.a.T[:, 0], 'g')
+            plt.legend(loc="upper left")
+            plt.show()
+            print(loss)
---
+++ neuralODEs/neuralODE.py
@@ -0,0 +1,129 @@
+import autograd.numpy as np
+from autograd import jacobian
+from scipy.integrate import solve_ivp
+
+class neuralODE():
+    #TODO write asserts
+    def __init__(self,fun,weights,x0s,x1s,t0,t1,M,delta,iter_max=100):
+        self.iter_max = iter_max
+        self.fun = fun
+        self.delta = delta
+        self.w = weights
+        self.x0s = x0s
+        self.x1s = x1s
+        self.x0 = x0s[0,:].flatten()
+        self.x1 = x1s[0,:].flatten()
+        self.t0 = t0
+        self.t1 = t1
+        self.M = M
+        self.t = np.linspace(t0,t1,M)
+
+        self.z = 0
+        self.a = 0
+
+
+    def fun_fwd(self,x,w):
+        "Output: fun at x, w"
+        "Input: fun input"
+        y = self.fun(x,w)
+        return y
+
+    def jacobian(self, x, t, w):
+        "Output: Jacobian of fun w.r.t x"
+        "Input: fun input"
+
+        t0 = self.t0
+        t1 = self.t1
+        M = self.M
+
+        f = lambda x: self.fun(x,w=self.w) #fun at weights = w
+        df_dx = lambda y: jacobian(f)(y)   #jacobian  of fun
+
+        t_span = np.linspace(t0, t1, M)
+        result = np.where((t - (t1-t0) / M <= t_span) * (t_span <= t + (t1-t0) / M))
+        try:
+            x_in = x[:,result[0][0]].flatten()
+        except:
+            x_in = x[:, 0].flatten()
+        J = df_dx(x_in)
+        return J
+
+    def forward(self):
+        "Output: Forward solution of ODE [z(0),...,z(T)]"
+        x0 = self.x0
+        weights = self.w
+
+        def f(t,x):
+            return self.fun_fwd(x,w=weights)
+
+        tau = [self.t[0],self.t[-1]]
+        sol= solve_ivp(f, tau, y0 = x0,t_eval =self.t)
+        self.z =   sol.y
+        self.t_z = sol.t
+        return None
+
+    def backward(self):
+        "Output: Backward solution of adjoint ODE [a(T),...,a(0)]"
+        def augmented(t,a):
+            J = self.jacobian(self.z,t,self.w)
+            return -a@J
+
+        sol = solve_ivp(augmented, [self.t[-1], self.t[0]],y0 = self.dloss_dx(), t_eval=np.flip(self.t))
+        self.a = sol.y
+        self.t_a = sol.t
+        return None
+
+    def dloss_dw(self):
+        "Output: Gradient of Loss dL_dw w.r.t weights"
+        t0 = self.t0
+        t1 = self.t1
+        M = self.M
+
+        df_dw = lambda x,w: jacobian(self.fun,1)(x,w)
+
+        dloss_dw = 0
+        for m in range(0,  M): #integration for dullies
+            z_in = self.z[:, M-m-1].flatten()
+            a_in = self.a[:, m].flatten()
+            J = df_dw(z_in, self.w)
+            l = a_in[0] * J[0] + a_in[1] * J[1]
+            dloss_dw -= l*(t1-t0)/M
+        return dloss_dw
+
+    def loss(self):
+        "Output: MSE loss"
+        return np.sum(np.power(self.x1 - self.z[:, -1].flatten(), 2))
+
+    def dloss_dx(self):
+        "Output: Gradient of MSE loss"
+        return 2*(self.x1 - self.z[:, -1].flatten())
+
+    def train_step(self):
+        "Perform single step of stochastic gradient descent"
+        delta = self.delta
+
+        w = self.w
+        loss = 0
+        batch = np.random.randint(0,len(self.x0s),100)
+        for k in batch :
+
+            self.x0 = self.x0s[k, :].flatten()
+            self.x1 = self.x1s[k, :].flatten()
+            self.forward()
+            self.backward()
+            gradient_w = self.dloss_dw()
+
+            w -= delta*gradient_w/len(batch) #update weights
+            loss += np.sum(np.power(self.x1 - self.z[:, -1].flatten(), 2))/len(self.x0s)
+
+        self.x0 = self.x0s[-1, :].flatten()
+        self.x1 = self.x1s[-1, :].flatten()
+        self.forward()
+        self.backward()
+        self.w = w
+        return loss
+
+
+
+
+
